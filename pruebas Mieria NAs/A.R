a82e5384146e780f3ad3264b1bb326a55e727934
gloknar <mipeeeerro@gmail.com>
2022-07-18 20:06:18 +0200

    .

---
+++ pruebas Mieria NAs/A.R
@@ -0,0 +1,7 @@
+# Para sacar gráficas de porcentajes de NAs en columnas
+# Sacado de https://cran.r-project.org/web/packages/naniar/vignettes/naniar-visualisation.html
+
+
+
+library("naniar")
+vis_miss(airquality)
